830f35fb61f94160bb400f1d371dc0967817a56b
yanghanwen <yanghanwen@36kr.com>
2015-03-20 16:17:02 +0800

    fix bugs

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 gulp build:cdn
-rsync -e "ssh -p 22" -rvltOD ./dist/* hanwen@119.254.111.175:~/kr-plus-dev/dist/m
-rsync -e "ssh -p 22" -rvltOD ./dist/* hanwen@119.254.111.175:~/kr-plus/dist/m
+#rsync -e "ssh -p 22" -rvltOD ./dist/* hanwen@119.254.111.175:~/kr-plus-dev/dist/m
+#rsync -e "ssh -p 22" -rvltOD ./dist/* hanwen@119.254.111.175:~/kr-plus/dist/m
+rsync -rvltOD ./dist/* dev05:/data/work/asset/m
+rsync -rvltOD ./dist/* dev06:/data/work/asset/m
+scp ./dist/index.html dev02:/data/work/tomcat_servers/krplus_index/m
+scp ./dist/index.html dev03:/data/work/tomcat_servers/krplus_index/m
