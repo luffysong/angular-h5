97bba1c4ba4dec3fa158ddb7f648d55ff9e489a3
yanghanwen <yanghanwen@36kr.com>
2015-06-02 13:36:13 +0800

    update deploy script

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-gulp build:cdn
-#rsync -e "ssh -p 22" -rvltOD ./dist/* hanwen@119.254.111.175:~/kr-plus-dev/dist/m
-#rsync -e "ssh -p 22" -rvltOD ./dist/* hanwen@119.254.111.175:~/kr-plus/dist/m
-rsync -rvltOD ./dist/* dev05:/data/work/asset/m
-rsync -rvltOD ./dist/* dev06:/data/work/asset/m
-scp ./dist/index.html dev04:/data/work/tomcat_servers/krplus_index/m
+gulp build:dev
+rsync -rvltOD ./dist/* dev04:/data/work/frontend/dev/36kr/krplus/dist/m
+
