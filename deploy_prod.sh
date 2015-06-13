c2014b6f2083944bfe0cb5be5ac5abfb4acc82ef
yanghanwen <yanghanwen@36kr.com>
2015-06-13 17:08:51 +0800

    发布

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 gulp build:prod
-rsync -rvltOD ./dist/* dev03:/data/work/frontend/test/36kr/krplus/dist/m
 
+rsync -rvltOD ./dist/* dev05:/data/work/asset/m
+rsync -rvltOD ./dist/* dev06:/data/work/asset/m
+
+
+#ssh dev06 'cd /data/work/tomcat_servers/krplus_index/m1;mv index.html index.html.$(date +%Y-%m-%d_%H:%M:%S).bak;exit;'
+#scp ./dist/index.html dev06:/data/work/tomcat_servers/krplus_index/m
+#
+#
+#rsync -rvltOD ./dist/* dev06:/data/work/frontend/prod/36kr/krplus/dist/m
+rsync -rvltOD ./dist/* dev05:/data/work/frontend/prod/36kr/krplus/dist/m
