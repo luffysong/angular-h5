f4529323784061130fba150a31b08762295d5bf5
yanghanwen <yanghanwen@36kr.com>
2015-07-09 09:06:08 +0800

    fix upload problem for upload

--- src/scripts/config/setting.js
+++ src/scripts/config/setting.js
@@ -2,7 +2,7 @@ window.kr = {};
 
 // 又拍云
 kr.upyun = {
-    api: 'http://v0.api.upyun.com',
+    api: '//v0.api.upyun.com',
     bucket: {
         name: 'krplus-pic',
         url: 'http://krplus-pic.b0.upaiyun.com'
