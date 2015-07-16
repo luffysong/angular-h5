f8a56bcad6745227ddea291171af0c58a603288f
yanghanwen <yanghanwen@36kr.com>
2015-07-16 12:36:22 +0800

    修复跟投人认证线上问题

--- deploy_prod_m1.sh
+++ deploy_prod_m1.sh
@@ -5,8 +5,8 @@ rsync -rvltOD ./dist/* dev05:/data/work/asset/m1
 rsync -rvltOD ./dist/* dev06:/data/work/asset/m1
 
 
-ssh dev06 'cd /data/work/tomcat_servers/krplus_index/m1;mv index.html index.html.$(date +%Y-%m-%d_%H:%M:%S).bak;exit;'
-scp ./dist/index.html dev06:/data/work/tomcat_servers/krplus_index/m1
+#ssh dev06 'cd /data/work/tomcat_servers/krplus_index/m1;mv index.html index.html.$(date +%Y-%m-%d_%H:%M:%S).bak;exit;'
+#scp ./dist/index.html dev06:/data/work/tomcat_servers/krplus_index/m1
 
 
 rsync -rvltOD ./dist/* dev06:/data/work/frontend/prod/36kr/krplus/dist/m1
