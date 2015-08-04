de52810d21c17853b388614cd3a694267a1ecb99
luxinyue <luxinyue@36kr.com>
2015-08-04 11:38:15 +0800

    fix test3 host config

--- src/scripts/config/env_test3.js
+++ src/scripts/config/env_test3.js
@@ -1,7 +1,7 @@
 window.projectEnvConfig = {
-    passportHost: 'passport.36kr.com',
-    krHost: 'staging.36kr.com',
-    rongHost: 'rongtest.36kr.com',
-    ucHost: 'uctest.36kr.com',
-    helpHost: 'helptest.36kr.com'
+    passportHost: 'passport3.36kr.com',
+    krHost: 'staging3.36kr.com',
+    rongHost: 'rongtest3.36kr.com',
+    ucHost: 'uctest3.36kr.com',
+    helpHost: 'helptest3.36kr.com'
 };
