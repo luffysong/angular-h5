f2c0624c0ed642b01190a91f77b0430718190021
luxinyue <luxinyue@36kr.com>
2015-08-04 10:50:40 +0800

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -577,9 +577,9 @@ gulp.task('remote:test3', function(){
 gulp.task('header', function(){
     var headers = {
 
-        test3: '//krplus-cdn.b0.upaiyun.com/common-module/common-header-test3/script.js',
-        test: '//krplus-cdn.b0.upaiyun.com/common-module/common-header-test/script.js',
-        dev: '//krplus-cdn.b0.upaiyun.com/common-module/common-header-dev/script.js',
+        test: '//huodong.36kr.com/common-module/common-header-test/script.js',
+        test3: '//huodong.36kr.com/common-module/common-header-test3/script.js',
+        dev: '//huodong.36kr.com/common-module/common-header-dev/script.js',
 
         prod: '//krplus-cdn.b0.upaiyun.com/common-module/common-header/script.js'
     };
