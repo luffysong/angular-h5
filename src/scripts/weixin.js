d4418f3b0f0b18b0e58fb0cb750b2c9cba902811
luxinyue <luxinyue@36kr.com>
2015-08-15 12:03:19 +0800

    分享 & 创业感言截取cid

--- src/scripts/weixin.js
+++ src/scripts/weixin.js
@@ -44,6 +44,8 @@
                     dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
                     success: function () {
                         // 用户确认分享后执行的回调函数
+
+
                     },
                     cancel: function () {
                         // 用户取消分享后执行的回调函数
