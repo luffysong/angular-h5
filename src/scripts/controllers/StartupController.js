804e22f73ac4b3406c6160b665d643c9acf5b2a6
SweetyCoder <sweetycoder@gmail.com>
2015-08-19 15:24:02 +0800

    微信关闭调试模式

--- src/scripts/controllers/StartupController.js
+++ src/scripts/controllers/StartupController.js
@@ -170,6 +170,7 @@ angular.module('defaultApp.controller').controller('startupController', [
         $scope.getCode = function(id, provider) {
             if(!id) return;
             $scope.provider = provider;
+            $scope.InitWeixin();
             if(!$scope.user.uid) {
                 $state.go('startupLogin');
             } else {
@@ -192,7 +193,7 @@ angular.module('defaultApp.controller').controller('startupController', [
                         if(err.code == 2002) {
                             $state.go('startupCompany');
                         } else {
-                            ErrorService.alert(err); 
+                            ErrorService.alert(err);
                         }
                     });
                 }, function(err) {
@@ -221,7 +222,7 @@ angular.module('defaultApp.controller').controller('startupController', [
                 if(!data.data.token)return;
 
                 wx.config({
-                    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                     appId: 'wxd3ea1a9a22815a8c', // 必填，公众号的唯一标识
                     timestamp: timestamp, // 必填，生成签名的时间戳
                     nonceStr: nonceStr, // 必填，生成签名的随机串
