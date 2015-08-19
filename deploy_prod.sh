50b351d59ad0e72262e57dfbbf01084bc7d53706
yanghanwen <yanghanwen@36kr.com>
2015-08-19 21:21:47 +0800

    update deploy script

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -2,10 +2,11 @@
 gulp build:prod
 
 rsync -rvltOD ./dist/* dev05:/data/work/asset/m
+rsync -rvltOD ./dist/* dev06:/data/work/asset/m
+
 ssh dev05 'cd /data/work/frontend/prod/36kr/krplus;tar zcf m$(date +%Y%m%d%H%M%S).tar.gz dist/m/;exit;'
 rsync -rvltOD ./dist/* dev05:/data/work/frontend/prod/36kr/krplus/dist/m
 
 
-rsync -rvltOD ./dist/* dev06:/data/work/asset/m
 ssh dev06 'cd /data/work/frontend/prod/36kr/krplus;tar zcf m$(date +%Y%m%d%H%M%S).tar.gz dist/m/;exit;'
 rsync -rvltOD ./dist/* dev06:/data/work/frontend/prod/36kr/krplus/dist/m
