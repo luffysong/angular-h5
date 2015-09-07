e11054e77b2bfbe2f91dec093d1117d2813ace2f
guoliyuan <guoliyuan@36kr.com>
2015-09-07 16:08:23 +0800

    返回上一页

--- src/scripts/controllers/FinacingSuccessController.js
+++ src/scripts/controllers/FinacingSuccessController.js
@@ -8,14 +8,15 @@ angular.module('defaultApp.controller').controller('FinacingSuccessController',
     function($scope, $rootScope, $stateParams, $location, loading, $modal, CompanyService, UserService, DictionaryService, $http, ErrorService, $timeout) {
         $scope.uid = UserService.getUID();
         $scope.stateParams = $stateParams;
-        // console.log($scope.stateParams);
-        if(!$stateParams.cid) return;
-        $scope.finacingList = {};
         // 返回上一页
         $scope.goBack = function(e) {
             e && e.preventDefault();
             history.go(-1);
         }
+        
+        if(!$stateParams.cid) return;
+        $scope.finacingList = {};
+        
         loading.show('finacingSuccess');
         CompanyService.ventureApply($scope.stateParams.cid, {
             type: $stateParams.from
