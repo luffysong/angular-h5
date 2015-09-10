3a523e35d758ee0a6152f7740767243b68ccaef8
luxinyue <luxinyue@36kr.com>
2015-09-10 10:35:14 +0800

    add 调试

--- src/scripts/services/AndroidUploadService.js
+++ src/scripts/services/AndroidUploadService.js
@@ -8,6 +8,13 @@ androidUploadFileCallback = function(){
 
 }
 
+function logdiv(msg){
+    if($('.logdiv').length == 0){
+        $('<div class="logdiv" style="position: fixed; top: 0;left: 0;width: 100px;height: 100px;background-color: #cccc99;z-index: 999;"></div>').appendTo('body')
+    }
+    $('.logdiv').html(msg)
+}
+
 angular.module('defaultApp.service').service('AndroidUploadService',
     function () {
 
@@ -18,11 +25,13 @@ angular.module('defaultApp.service').service('AndroidUploadService',
 
                 return function(e){
                     if(!window.kr36 || !window.kr36.chooseFile){
+                        logdiv('没有注入方法')
                         return function(){};
                     }
 
                     e.preventDefault();
                     setTimeout(function(){
+                        logdiv('有注入方法，立刻调用')
                         window.kr36.chooseFile();
                     },100)
                 }
