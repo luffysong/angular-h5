3ae0c102f1bfa1cd807437d95a06e529ccec2253
张屈 <joneyphair@qq.com>
2015-09-17 16:10:29 +0800

    feature/h5－优化CreateCompanyController文件，剥离suggestService文件

--- src/scripts/controllers/CreateCompanyController.js
+++ src/scripts/controllers/CreateCompanyController.js
@@ -176,14 +176,14 @@ angular.module('defaultApp.controller').controller('CreateCompanyController', [
 
         // add new company
         $scope.formData.operationStatus = 'OPEN';
-        
+
         $scope.addCompany = function (name) {
             $scope.opNext = 0;
             $scope.formData.name = name;
             $scope.formData.website = '';
             $scope.formData.brief = '';
             $scope.formData.logo = '';
-            
+
             $scope.formData.bizCardLink = '';
             $scope.formData.cid = null;
             $scope.temp_logo = '';
@@ -616,12 +616,16 @@ angular.module('defaultApp.controller').controller('CreateCompanyController', [
             }
         }
 
-    }]).service('SuggestService', [
+    }]);
+
+
+
+/*service('SuggestService', [
     '$http',
     'BasicService',
     function ($http, BasicService) {
         var service = BasicService("/api/suggest/company", {})
 
         return service;
-    }])
+    }])*/
 
