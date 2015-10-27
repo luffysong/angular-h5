9273160629bfb2791a6cce5afe73943b8cc79971
luxinyue <luxinyue@36kr.com>
2015-10-27 16:07:09 +0800

    fix prod

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,54 +1,15 @@
 #!/bin/bash
 
 gulp build:prod
+rsync -rvltOD ./dist/* dev05:/data/work/asset/m
+rsync -rvltOD ./dist/* dev06:/data/work/asset/m
 
-####### CDN
-cdnstr="/data/work/asset/m"
-#site1
-rsync -rvltOD ./dist/* ali-rong-proxy-01:$cdnstr
-rsync -rvltOD ./dist/* ali-rong-php-01:$cdnstr
-rsync -rvltOD ./dist/* ali-rong-tomcat-01:$cdnstr
-
-#site2
-rsync -rvltOD ./dist/* ali-rong-proxy-02:$cdnstr
-rsync -rvltOD ./dist/* ali-rong-php-02:$cdnstr
-rsync -rvltOD ./dist/* ali-rong-tomcat-02:$cdnstr
-
-
-###### 业务逻辑
-pathstr="/data/work/frontend/prod/36kr/krplus/dist/m"
-if [ $1 = 'site1' ]; then
-    rsync -rvltOD ./dist/* ali-rong-proxy-01:$pathstr
-    rsync -rvltOD ./dist/* ali-rong-php-01:$pathstr
-    rsync -rvltOD ./dist/* ali-rong-tomcat-01:$pathstr
-elif [ $1 = 'site2' ]; then
-    rsync -rvltOD ./dist/* ali-rong-proxy-02:$pathstr
-    rsync -rvltOD ./dist/* ali-rong-php-02:$pathstr
-    rsync -rvltOD ./dist/* ali-rong-tomcat-02:$pathstr
-elif [ $1 = 'all' ]; then
-    rsync -rvltOD ./dist/* ali-rong-proxy-01:$pathstr
-    rsync -rvltOD ./dist/* ali-rong-php-01:$pathstr
-    rsync -rvltOD ./dist/* ali-rong-tomcat-01:$pathstr
-    rsync -rvltOD ./dist/* ali-rong-proxy-02:$pathstr
-    rsync -rvltOD ./dist/* ali-rong-php-02:$pathstr
-    rsync -rvltOD ./dist/* ali-rong-tomcat-02:$pathstr
-fi
-
-
-
-
-
-
-#gulp build:prod
-#rsync -rvltOD ./dist/* dev05:/data/work/asset/m
-#rsync -rvltOD ./dist/* dev06:/data/work/asset/m
-#
-#oldIFS="$IFS"
-#IFS="&"
-#arr=("$1")
-#for i in ${arr[@]}
-#do
-#ssh dev${i} 'cd /data/work/frontend/prod/36kr/krplus;tar zcf m$(date +%Y%m%d%H%M%S).tar.gz dist/m/;exit;'
-#rsync -rvltOD ./dist/* dev${i}:/data/work/frontend/prod/36kr/krplus/dist/m
-#done;
-#IFS="$oldIFS"
+oldIFS="$IFS"
+IFS="&"
+arr=("$1")
+for i in ${arr[@]}
+do
+ssh dev${i} 'cd /data/work/frontend/prod/36kr/krplus;tar zcf m$(date +%Y%m%d%H%M%S).tar.gz dist/m/;exit;'
+rsync -rvltOD ./dist/* dev${i}:/data/work/frontend/prod/36kr/krplus/dist/m
+done;
+IFS="$oldIFS"
