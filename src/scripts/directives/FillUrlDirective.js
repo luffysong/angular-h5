e4f681df238a45d06b98fe141ce0de6f8635ea99
luxinyue <luxinyue@36kr.com>
2015-11-02 00:27:41 +0800

    url校验

--- src/scripts/directives/FillUrlDirective.js
+++ src/scripts/directives/FillUrlDirective.js
@@ -5,7 +5,8 @@
 var angular = require('angular');
 
 angular.module('defaultApp.directive').directive('fillUrl',
-    function ($location) {
+    function ($location, DictionaryService) {
+        var urlArr = DictionaryService.getDict('CompanyUrlBlacklist');
         return {
             restrict: 'AE',
             require: '^?form',
@@ -13,25 +14,39 @@ angular.module('defaultApp.directive').directive('fillUrl',
                 if(!attrs.ngModel){
                     return;
                 }
+                scope.$watch(attrs.ngModel, function(){
+                    if(scope.$eval(attrs.ngModel) == ''){
+                        formCtrl[element.attr('name')].$setPristine();
+                    }
+                })
                 element.bind('focus', function(){
-                    setTimeout(function(){
-                        var val = scope.$eval(attrs.ngModel);
-                        if(!val){
+                    formCtrl[element.attr('name')].$setValidity('urlblacklist', true);
+                    var val = scope.$eval(attrs.ngModel);
+                    if(!val){
+                        setTimeout(function(){
                             scope.$apply(function(){
                                 scope.$eval(attrs.ngModel+"='http://'");
-                                formCtrl[element.attr('name')].$setPristine();
-                            });
-                        }
-                    },0);
+                            })
+                        },0)
+                    }
                 });
                 element.bind('blur', function(){
                     setTimeout(function(){
                         var val = scope.$eval(attrs.ngModel);
+                        if(!val) return;
                         if(val=='http://'){
                             scope.$apply(function(){
                                 scope.$eval(attrs.ngModel+"=''");
                                 formCtrl[element.attr('name')].$setPristine();
                             });
+                        }
+                        for (var i = 0; i < urlArr.length; i++){
+                            if(val.indexOf(urlArr[i]) > -1 ){
+                                scope.$apply(function(){
+                                    formCtrl[element.attr('name')].$setValidity('urlblacklist', false);
+                                });
+                                break;
+                            }
                         }
                     },0);
 
