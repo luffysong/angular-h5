4784aac3d2f6b3c089ea50dbf6e47693b48111ca
zhouxiao <zhouxiao@36kr.com>
2015-11-02 16:29:15 +0800

    字体add

--- src/scripts/filters/index.js
+++
@@ -1,8 +0,0 @@
-require('./Image');
-require('./InvestorLink');
-require('./MoneyUnit');
-require('./Substr');
-require('./UnitConversion');
-require('./WanNum');
-require('./WanText');
-require('./default');
