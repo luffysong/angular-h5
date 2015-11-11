08a27b88e7b77d8e4ae3e9f2fcdd4d824deb0b50
李京 <lijing@36kr.com>
2015-11-11 16:11:07 +0800

    feature 跟投人拉新活动

--- src/scripts/controllers/syndicatesInviteController.js
+++ src/scripts/controllers/syndicatesInviteController.js
@@ -7,5 +7,9 @@ var angular = require('angular');
 angular.module('defaultApp.controller').controller('SyndicatesInviteController',
     function($scope, $state, $stateParams, $modal, notify, $timeout, loading, UserService, ErrorService, DictionaryService, CoInvestorService) {
         loading.show("syndicatesInvite");
+
+        $timeout(function() {
+            loading.hide("syndicatesInvite");
+        }, 3000);
     });
 
