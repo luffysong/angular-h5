a91d05fa43265fdfe1d12058443300a763cb5300
李京 <lijing@36kr.com>
2015-11-13 17:00:38 +0800

    feature 跟投人拉新活动

--- src/scripts/controllers/syndicatesInviteController.js
+++ src/scripts/controllers/syndicatesInviteController.js
@@ -5,7 +5,7 @@
 var angular = require('angular');
 
 angular.module('defaultApp.controller').controller('SyndicatesInviteController',
-    function($scope, $state, $stateParams, $modal, notify, $timeout, loading, UserService, CrowdFundingService, ErrorService, DictionaryService, CoInvestorService) {
+    function($scope, $state, $stateParams, $modal, notify, $timeout, $interval, loading, UserService, CrowdFundingService, ErrorService, DictionaryService, CoInvestorService) {
         loading.show("syndicatesInvite");
 
         $scope.uid = UserService.getUID();
@@ -16,9 +16,36 @@ angular.module('defaultApp.controller').controller('SyndicatesInviteController',
         },function(data) {
             $scope.couponList = data.data;
             loading.hide("syndicatesInvite");
+            $scope.scroll();
         },function(err) {
             ErrorService.alert(err);
         });
+
+        var timer;
+        $scope.scroll = function() {
+            $timeout(function() {
+                timer = $interval(function() {
+                    var $recordsList = $('.records-list');
+                    var $recordsItem = $recordsList.find('li');
+                    var outterHeight = $recordsList.height();
+                    var innerHeight = $recordsItem.height() * $recordsItem.length;
+
+                    var scrollTop = $recordsList.scrollTop();
+                    if(outterHeight + scrollTop >= innerHeight){
+                        scrollTop -= (innerHeight / 2) + 10;
+                    } else {
+                        scrollTop++;
+                    }
+                    $recordsList.scrollTop(scrollTop);
+                }, 30);
+            }, 1000);
+        };
+
+        $scope.scroll();
+
+        $scope.stop = function(){
+            $interval.cancel(timer);
+        };
 
         $scope.viewInviteRecord = function($event) {
             $event.preventDefault();
