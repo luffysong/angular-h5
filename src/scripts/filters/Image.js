1a19d74d4f3024200dbec6a978fb8d0a8bda3310
李京 <lijing@36kr.com>
2015-11-16 15:03:54 +0800

    feature 跟投人拉新活动

--- src/scripts/filters/Image.js
+++ src/scripts/filters/Image.js
@@ -7,7 +7,7 @@ var angular = require('angular');
 var defaults = {
     avatar: 'http://krplus.b0.upaiyun.com/default_avatar.png',
     logo: 'http://krplus.b0.upaiyun.com/default_logo.png',
-    user: 'styles/images/invite/avatar.png'
+    user: 'http://krplus-pic.b0.upaiyun.com/201511/16070248/kopb6thbg3opudsr.png'
 };
 var sizeArr = [20,30,50,70,100,120,200,500,800];
 
