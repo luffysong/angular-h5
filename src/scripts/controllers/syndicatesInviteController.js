49d755ad881ecbbca5e21cf6923b9900ae20d57f
李京 <lijing@36kr.com>
2015-11-16 16:17:18 +0800

    feature 跟投人拉新活动

--- src/scripts/controllers/syndicatesInviteController.js
+++ src/scripts/controllers/syndicatesInviteController.js
@@ -25,12 +25,19 @@ angular.module('defaultApp.controller').controller('SyndicatesInviteController',
                     $scope.permit = "coInvestor";
                 } else {
                     if(data && data.code == 4031) {
-                        $state.go('syndicatesValidate');
+                        $state.go('syndicatesValidate', {
+                            inviter_id: $stateParams.id
+                        });
                     } else {
                         CrowdFundingService["audit"].get({
                             id: "co-investor",
                             submodel: "info"
                         }, function(data) {
+                            if(data.cert_info && data.cert_info.length == 0) {
+                                $state.go('syndicatesValidate', {
+                                    inviter_id: $stateParams.id
+                                });
+                            }
                         }, function(err) {
                             if(err.code == 1002) {
                                 $scope.permit = "preInvestor";
