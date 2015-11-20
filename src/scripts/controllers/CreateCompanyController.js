9daf4d8f0e5529f2d1783049ba899efc6cd9b7bb
luxinyue <luxinyue@36kr.com>
2015-11-20 19:05:54 +0800

    修改创建公司成功后跳转地址

--- src/scripts/controllers/CreateCompanyController.js
+++ src/scripts/controllers/CreateCompanyController.js
@@ -466,7 +466,7 @@ angular.module('defaultApp.controller').controller('CreateCompanyController', [
                 'mode': 'direct'
             }, angular.copy($scope.formData), function (data) {
                 if($scope.jisu){
-                    location.href = '/m/j3companycreate.html?cid='+data.id
+                    location.href = '/m/j4companycreate.html?cid='+data.id
                 }else{
                     location.hash = "/company_create_apply"
                 }
