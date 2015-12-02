09656a62c12459f0b4537ed9a3bce124dc25c8c4
李京 <lijing@36kr.com>
2015-12-02 11:08:00 +0800

    feature 测试环境+789

--- src/scripts/config/env_test7.js
+++ src/scripts/config/env_test7.js
@@ -1,7 +1,7 @@
 window.projectEnvConfig = {
     passportHost: 'passport.36kr.com',
     krHost: 'staging.36kr.com',
-    rongHost: 'rongtest6.36kr.com',
-    ucHost: 'uctest6.36kr.com',
+    rongHost: 'rongtest7.36kr.com',
+    ucHost: 'uctest7.36kr.com',
     helpHost: 'helptest.36kr.com'
 };
--- src/scripts/config/env_test9.js
+++ src/scripts/config/env_test9.js
@@ -1,7 +1,7 @@
 window.projectEnvConfig = {
     passportHost: 'passport.36kr.com',
     krHost: 'staging.36kr.com',
-    rongHost: 'rongtest6.36kr.com',
-    ucHost: 'uctest6.36kr.com',
+    rongHost: 'rongtest9.36kr.com',
+    ucHost: 'uctest9.36kr.com',
     helpHost: 'helptest.36kr.com'
 };
