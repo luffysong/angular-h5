22a94782d909331d77035d973f33c4ab20054981
liyi <liyi@36kr.com>
2015-12-28 16:16:23 +0800

    领券页面 添加微信分享

--- src/scripts/controllers/syndicatesGiftController.js
+++ src/scripts/controllers/syndicatesGiftController.js
@@ -46,6 +46,57 @@ angular.module('defaultApp.controller').controller('SyndicatesGiftController',
                     });
                 }
             });
+
+            // 获取用户信息
+            UserService.basic.get({
+                id: $scope.uid
+            }, function(data){
+                $scope.uname = data.name;
+            }, function(err) {
+                console.log(err);
+            });
+
         }
+
+        // 分享
+        $scope.shareDesc = '';
+        $scope.share = function($event) {
+            $event.preventDefault();
+            $modal.open({
+                windowClass: 'invite-share-window',
+                templateUrl: 'templates/syndicates/invite/pop-invite-share.html',
+                controller: [
+                    '$scope', '$modalInstance',
+                    function($scope, $modalInstance) {
+                        $scope.ok = function() {
+                            $modalInstance.dismiss();
+                        }
+                    }]
+            });
+        };
+
+        $scope.$watch('uname', function(from) {
+            if($scope.isLogin) {
+                if(from) {
+                    $scope.shareTitle = '【36氪限时福利】我是' + from + '，请你来拿1000元投资现金，一起做股东！';
+                    $scope.shareDesc = '投资新锐互联网公司，获得高收益！';
+                    $scope.formUserNmae = from;
+                } else {
+                    $scope.shareTitle = '【限时福利】来36氪，做股东，立得1000元！';
+                    $scope.shareDesc = '投资新锐互联网公司，获得高收益！';
+                }
+            } else {
+                $scope.shareTitle = '【限时福利】来36氪，做股东，立得1000元！';
+                $scope.shareDesc = '投资新锐互联网公司，获得高收益！';
+            }
+            window.WEIXINSHARE = {
+                shareTitle: $scope.shareTitle,
+                shareDesc: $scope.shareDesc,
+                shareImg: 'http://krplus-pic.b0.upaiyun.com/201512/04094947/x0bjnbw0hra8nne8.jpg',
+                shareHref: location.protocol + '//' + location.host + '/m/#/syndicatesInvite?id=' + ($scope.isLogin ? $scope.uid : $stateParams.id)
+            };
+            InitWeixin();
+        });
+
     });
 
