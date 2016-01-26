4b7997d1127dd451610ab9df91c0df0c0157d4b5
songkaiyu <songkaiyu@36kr.com>
2016-01-26 12:52:06 +0800

    发布文件更改

--- deploy_prod_aliyun.sh
+++ deploy_prod_aliyun.sh
@@ -8,11 +8,13 @@ cdnstr="/data/work/asset/m"
 rsync -rvltOD ./dist/* ali-rong-proxy-01:$cdnstr
 rsync -rvltOD ./dist/* ali-rong-php-01:$cdnstr
 rsync -rvltOD ./dist/* ali-rong-tomcat-01:$cdnstr
+rsync -rvltOD ./dist/* ali-rong-api-01:$cdnstr
 
 #site2
 rsync -rvltOD ./dist/* ali-rong-proxy-02:$cdnstr
 rsync -rvltOD ./dist/* ali-rong-php-02:$cdnstr
 rsync -rvltOD ./dist/* ali-rong-tomcat-02:$cdnstr
+rsync -rvltOD ./dist/* ali-rong-api-02:$cdnstr
 
 
 ###### 业务逻辑
@@ -21,17 +23,21 @@ if [ $1 = 'site1' ]; then
     rsync -rvltOD ./dist/* ali-rong-proxy-01:$pathstr
     rsync -rvltOD ./dist/* ali-rong-php-01:$pathstr
     rsync -rvltOD ./dist/* ali-rong-tomcat-01:$pathstr
+    rsync -rvltOD ./dist/* ali-rong-api-01:$pathstr
 elif [ $1 = 'site2' ]; then
     rsync -rvltOD ./dist/* ali-rong-proxy-02:$pathstr
     rsync -rvltOD ./dist/* ali-rong-php-02:$pathstr
     rsync -rvltOD ./dist/* ali-rong-tomcat-02:$pathstr
+    rsync -rvltOD ./dist/* ali-rong-api-02:$pathstr
 elif [ $1 = 'all' ]; then
     rsync -rvltOD ./dist/* ali-rong-proxy-01:$pathstr
     rsync -rvltOD ./dist/* ali-rong-php-01:$pathstr
     rsync -rvltOD ./dist/* ali-rong-tomcat-01:$pathstr
+    rsync -rvltOD ./dist/* ali-rong-api-01:$pathstr
     rsync -rvltOD ./dist/* ali-rong-proxy-02:$pathstr
     rsync -rvltOD ./dist/* ali-rong-php-02:$pathstr
     rsync -rvltOD ./dist/* ali-rong-tomcat-02:$pathstr
+    rsync -rvltOD ./dist/* ali-rong-api-02:$pathstr
 elif [ $1 = 'sim' ]; then
     rsync -rvltOD ./dist/* ali-rong-sim-01:$pathstr
 fi
