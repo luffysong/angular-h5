6342afa4c42c4c427ca3acbd985b6daa7d49e3ac
songkaiyu <songkaiyu@36kr.com>
2016-06-23 17:30:43 +0800

    fix(项目集): 在APP内点击logo或名字，打开相应项目

---
+++ src/scripts/services/hybrid.service.js
@@ -0,0 +1,34 @@
+
+var angular = require('angular');
+angular.module('defaultApp.service').service('hybrid', function () {
+
+    var _this = this;
+    this.isInApp = !!navigator.userAgent.match(/36kr/) || window.isAppAgent;
+    this.openProject = openProject;
+    this.open = open;
+
+    function call(url) { //定义startApp函数
+        var ifr = document.createElement('iframe');
+        if (_this.isInApp) {
+            ifr.src = url;
+            ifr.style.display = 'none';
+            document.body.appendChild(ifr);
+            window.setTimeout(function () {
+                document.body.removeChild(ifr);
+                ifr = null;
+            }, 200);
+        }
+    }
+
+    function buildUrl(path) {
+        return 'krtou:/' + path;
+    }
+
+    function open(path) {
+        call(buildUrl(path));
+    }
+
+    function openProject(id) {
+        call(buildUrl('/company/' + id));
+    }
+});
