b341f859fc8dd855551030dfa8c744fe9ffe11e8
songkaiyu <songkaiyu@36kr.com>
2016-06-30 01:02:55 +0800

    fix(投资人认证优化): 依据字段在表单的顺序来显示错误提示 +2
    第一次修改时仅仅是将error-wrapper中的错误的z-index reverse，
    然而 error-wrapper中的错误并不是按照表单元素顺序排列的。
    此处将表单中控件的顺序与错误提示关联起来,然后使用1000减去
    元素在表单中的次序作为它的z-index

--- src/scripts/directives/ErrorWrapperDirective.js
+++ src/scripts/directives/ErrorWrapperDirective.js
@@ -13,9 +13,47 @@ angular.module('defaultApp.directive').directive('errorWrapper',
             scope: {
                 errorGroup: '=errorGroup'
             },
+            bindToController: true,
+            controllerAs: 'vm',
             link: function (scope, element, attrs, formCtrl) {
-                scope.formCtrl = formCtrl;
-            }
+                var vm = scope.vm;
+                vm.formCtrl = formCtrl;
+                vm.formName = formCtrl.$name;
+                vm.form = document.getElementsByName(formCtrl.$name)[0];
+            },
+
+            controller: errorWrapperController
         };
     }
 );
+
+function errorWrapperController() {
+    var vm = this;
+    var BASE_Z_INDEX = 1000;
+    vm.getZindex = getZindex;
+
+    // function reorderError(name) {
+    //     if (!name)return;
+    //     var form = document.getElementsByName(name);
+    //     var $controls = $('input select', form);
+    //     var errorGroupCopy = angular.copy(vm.errorGroup);
+    //     var errors = [];
+    //     $controls.each(function lockError(ele) {
+    //         errors.concat(errorGroupCopy.filter(function compareName(data) {
+    //             errorGroupCopy.remove(data);
+    //             return data.name === ele.name;
+    //         }));
+    //     });
+    //
+    //     errors.reverse();
+    //     return errors;
+    // }
+
+    function getZindex(field) {
+        var controls = $('input,select', vm.form).toArray();
+        var currentControl = vm.form[field];
+        if (!currentControl) return -1;
+        return BASE_Z_INDEX - controls.indexOf(currentControl);
+    }
+
+}
