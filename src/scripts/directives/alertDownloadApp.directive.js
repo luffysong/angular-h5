d67ff7d9d0b07736fd88a44c91c0d36e15b5b9fd
佟德慧 <tongdehui@36kr.com>
2016-07-15 13:06:06 +0800

    fix: 调整controller写法

--- src/scripts/directives/alertDownloadApp.directive.js
+++ src/scripts/directives/alertDownloadApp.directive.js
@@ -9,68 +9,80 @@ angular.module('defaultApp.directive').directive('alertDownloadApp', alertDownlo
 function alertDownloadApp($parse) {
     return {
         restrict: 'AE',
-        controller: function ($scope, $modal) {
-            var isInApp = !!navigator.userAgent.match(/36kr/) || window.isAppAgent;
-            $scope.checkApp = function (pid) {
-                if (pid && (!isInApp)) {
-                    pop(pid);
-                }
-            };
-
-            // 下载app弹窗
-            function pop(pid) {
-                $modal.open({
-                    templateUrl: 'templates/demos/downLoadPop.html',
-                    windowClass: 'down_load_pop_wrap',
-                    controller: modalController,
-                    controllerAs: 'vm',
-                    resolve: {
-                        scope: function () {
-                            return $scope;
-                        },
-
-                        obj: function () {
-                            return {
-                                pid:pid,
-                            };
-                        }
-                    }
-                }).result.then(allowScroll).catch(allowScroll);
-            }
-
-            function allowScroll() {
-                $(document).unbind('touchmove');
-            }
-
-            modalController.$inject = ['$scope', '$modalInstance', 'scope', 'ErrorService', 'obj'];
-            function modalController($scope, $modalInstance, scope, ErrorService, obj) {
-
-                var vm = this;
-                var request = {
-                    pid:obj.pid
-                };
-                vm.href = 'https://rong.36kr.com/m/redirect.html?ktm_source=xiangmuji' + request.pid + '&redirectUrl=http%3A%2F%2Fa.app.qq.com%2Fo%2Fsimple.jsp%3Fpkgname%3Dcom.android36kr.investment';
-                vm.cancel = function () {
-
-                    //关闭弹窗
-                    $modalInstance.dismiss();
-                };
-
-                $(document).on('touchmove', function () {
-                    event.preventDefault();
-                });
-            }
-        },
-
-        link: function ($scope, element, attr) {
+        controller: popController,
+        controllerAs: 'vmAlertDownloadApp',
+        bindToController: true,
+        link: function postLink($scope, element, attr) {
             var pidExp = attr.alertDownloadApp;
             var pidFn = $parse(pidExp, null, true);
+            var vm = $scope.vmAlertDownloadApp;
 
             element.click(function openApp(e) {
                 e.preventDefault();
                 var pid = pidFn($scope);
-                $scope.checkApp(pid);
+                vm.checkApp(pid);
             });
         }
     };
+}
+
+function popController($scope, $modal, hybrid) {
+    var vm = this;
+    vm.isInApp = hybrid.isInApp;
+    vm.checkApp = checkApp;
+
+    function checkApp(pid) {
+        if (pid && (!vm.isInApp)) {
+            pop(pid);
+        }
+    }
+
+    // 下载app弹窗
+    function pop(pid) {
+        $modal.open({
+            templateUrl: 'templates/demos/downLoadPop.html',
+            windowClass: 'down_load_pop_wrap',
+            controller: modalController,
+            controllerAs: 'vm',
+            resolve: {
+                scope: function () {
+                    return $scope;
+                },
+
+                obj: function () {
+                    return {
+                        pid:pid,
+                    };
+                }
+            }
+        }).result.then(allowScroll).catch(allowScroll);
+    }
+}
+
+function allowScroll() {
+    window.removeEventListener('touchmove', preventDefault, false);
+}
+
+modalController.$inject = ['$scope', '$modalInstance', 'scope', 'ErrorService', 'obj'];
+function modalController($scope, $modalInstance, scope, ErrorService, obj) {
+
+    var vm = this;
+    vm.pid = obj.pid;
+    vm.cancel = cancel;
+
+    var hrefFront = 'https://rong.36kr.com/m/redirect.html?ktm_source=xiangmuji';
+    var hrefEnd = '&redirectUrl=http%3A%2F%2Fa.app.qq.com%2Fo%2Fsimple.jsp%3Fpkgname%3Dcom.android36kr.investment';
+    vm.href = hrefFront + vm.pid + hrefEnd;
+
+    function cancel() {
+
+        //关闭弹窗
+        $modalInstance.dismiss();
+    }
+
+    window.addEventListener('touchmove', preventDefault, false);
+}
+
+function preventDefault() {
+    event.preventDefault();
 }
