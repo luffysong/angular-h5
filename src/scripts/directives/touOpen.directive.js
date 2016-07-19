841f303a676eedeedce49652acd31e70a44f6681
songkaiyu <songkaiyu@36kr.com>
2016-07-19 12:03:33 +0800

    feat(项目集优化): 项目集优化
    1、调整字体大小
    2、增加加载loading效果
    3、增加打开native界面 loading效果

--- src/scripts/directives/touOpen.directive.js
+++ src/scripts/directives/touOpen.directive.js
@@ -6,7 +6,7 @@ var angular = require('angular');
 
 angular.module('defaultApp.directive').directive('touOpen', touOpen);
 
-function touOpen(hybrid) {
+function touOpen(hybrid, loading) {
     return {
         restrict: 'AE',
         scope:{
@@ -16,6 +16,11 @@ function touOpen(hybrid) {
         link: function (scope, element) {
             var path = scope.path;
             var pid = scope.pid;
+            loading.hide('openApp');
+            if (!hybrid.isInApp) {
+                return;
+            }
+
             element.click(function openApp(e) {
                 e.preventDefault();
                 if (pid) {
@@ -23,7 +28,19 @@ function touOpen(hybrid) {
                 } else {
                     hybrid.open(path);
                 }
+
+                loadingUI();
             });
         }
     };
+
+    function loadingUI() {
+        loading.show('openApp');
+        window.addEventListener('pagehide', pagehide, false);
+    }
+
+    function pagehide(loading) {
+        loading.hide('openApp');
+        window.removeEventListener('pagehide', pagehide);
+    }
 }
