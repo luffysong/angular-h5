9b03885598880d007441b83e0c16cfcf3472edc9
songkaiyu <songkaiyu@36kr.com>
2016-07-19 16:55:38 +0800

    fix(项目集): 打开项目显示loading状态，选中项目，更改背景

--- src/scripts/directives/touOpen.directive.js
+++ src/scripts/directives/touOpen.directive.js
@@ -6,7 +6,7 @@ var angular = require('angular');
 
 angular.module('defaultApp.directive').directive('touOpen', touOpen);
 
-function touOpen(hybrid, loading) {
+function touOpen(hybrid, loading, $timeout) {
     return {
         restrict: 'AE',
         scope:{
@@ -17,30 +17,35 @@ function touOpen(hybrid, loading) {
             var path = scope.path;
             var pid = scope.pid;
             loading.hide('openApp');
+
             if (!hybrid.isInApp) {
                 return;
             }
 
             element.click(function openApp(e) {
                 e.preventDefault();
+                loadingUI(scope);
                 if (pid) {
                     hybrid.openProject(pid);
                 } else {
                     hybrid.open(path);
                 }
 
-                loadingUI();
             });
         }
     };
 
-    function loadingUI() {
-        loading.show('openApp');
-        window.addEventListener('pagehide', pagehide, false);
+    function loadingUI(scope) {
+        scope.$evalAsync(function showLoading() {
+            loading.show('openApp');
+        });
+
+        function pagehide() {
+            loading.hide('openApp');
+        }
+
+        $timeout(pagehide, 2000);
+
     }
 
-    function pagehide(loading) {
-        loading.hide('openApp');
-        window.removeEventListener('pagehide', pagehide);
-    }
 }
