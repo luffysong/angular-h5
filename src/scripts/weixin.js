61396bee63156bb6aca1b0a10424aded63cab55d
songkaiyu <songkaiyu@36kr.com>
2016-08-09 17:43:09 +0800

    fix: 发送给好友时强制 图片强制使用http协议

--- src/scripts/weixin.js
+++ src/scripts/weixin.js
@@ -77,7 +77,8 @@
                     title: WEIXINSHARE.shareTitle, // 分享标题
                     desc: WEIXINSHARE.shareDesc, // 分享描述
                     link: WEIXINSHARE.shareHref || location.href, // 分享链接
-                    imgUrl: WEIXINSHARE.shareImg || 'http://d.36kr.com/assets/36kr.png', // 分享图标
+                    imgUrl: (WEIXINSHARE.shareImg ||
+                    'http://sta.36krcnd.com/common-module/common-header/images/app_logo.jpg').replace('https', 'http'),
                     type: 'link', // 分享类型,music、video或link，不填默认为link
                     dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
                     success: function () {
