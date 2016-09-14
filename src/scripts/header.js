9213347b2c7ea8c163b3e75d4fbfe3dbde1f06bd
佟德慧 <tongdehui@36kr.com>
2016-09-14 15:58:44 +0800

    feat: 增加唤醒app微信引导打开页、顶部banner替换

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,4 +1,5 @@
 (function setHeader(win, doc) {
+    var cid;
 
     function headerOnload() {
         if (allowRemoveHeader()) {
@@ -25,10 +26,35 @@
     function replaceBanner() {
         var html = $('#topBanner').html();
         $('.app-container.J_appDownloadWrapper').replaceWith(html);
+        if (/company\/\d+/.test(location.hash)) {
+            $('#bannerCompany').show();
+            $('#bannerOther').hide();
+
+            var split = location.href.split('company/');
+            if (split[1]) {
+                cid = split[1];
+                $('#goDownLoad').attr('href', 'http://36kr.com/company/' + cid + '?fromShare=1&ktm_source=rong.h5.download.' + cid);
+            } else {
+                $('#goDownLoad').attr('href', 'http://huodong.36kr.com/h5/investor-app/hub.html?ktm_source=rongh5dingbu');
+            }
+
+        } else {
+            $('#bannerCompany').hide();
+            $('#bannerOther').show();
+        }
     }
 
     function writeHeader() {
         doc.write('<script src="%%%headerSrc%%%" onload="headerOnload()" name="rong" ><\/script>');
+    }
+
+    function download() {
+        var userSystem = navigator.userAgent; //userAgent
+        if (userSystem.indexOf('Android') > -1) {
+            krtracker('trackEvent', 'click', 'android.h5.rongh5dingbu.download.' + cid);
+        }else if (/iphone/i.test(userSystem)) {
+            krtracker('trackEvent', 'click', 'ios.h5.rongh5dingbu.download.' + cid);
+        }
     }
 
     writeHeader();
