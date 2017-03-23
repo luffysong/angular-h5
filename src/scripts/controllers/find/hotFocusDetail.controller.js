677b0fc81059d9d9fdb0e42523e7ae9765867563
sweeetcc <sweeetcc@gmail.com>
2017-03-23 17:25:20 +0800

    fix: 根据原型添加图标

--- src/scripts/controllers/find/hotFocusDetail.controller.js
+++ src/scripts/controllers/find/hotFocusDetail.controller.js
@@ -5,4 +5,52 @@ angular.module('defaultApp.controller')
 function HotFocusDetailController(loading) {
     var vm = this;
     loading.hide('findLoading');
+    // 页面 title
+    document.title = '热点详情';
+    // 图表 1 配置
+    vm.chartConfig1 = {
+        options: {
+            chart: {
+                type: 'line',
+            },
+        },
+        title: {
+            text: '同比增长',
+        },
+        xAxis: {
+            categories: ['二月', '三月', '四月', '五月', '六月', '七月', '八月'],
+        },
+        yAxis: {
+            title: null,
+        },
+        series: [{
+            name: '增长',
+            data: [10, 13, 25, 3, 8, 30, 15],
+        }],
+        legend: {
+            enabled: false,
+        },
+    };
+    // 图表 2 配置
+    vm.chartConfig2 = {
+        options: {
+            chart: {
+                type: 'bar',
+            },
+        },
+        title: {
+            text: '竞品搜索量排行',
+        },
+        xAxis: {
+            categories: ['36氪', '3W coffee', 'IT 橘子'],
+            title: null,
+        },
+        yAxis: {
+            title: null,
+        },
+        series: [{
+            name: '搜索量',
+            data: [10, 8, 6],
+        },],
+    };
 }
