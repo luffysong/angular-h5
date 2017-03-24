31d260b56d741094ca8f023222ce7f64e8a71378
sweeetcc <sweeetcc@gmail.com>
2017-03-24 15:22:04 +0800

    fix: 图标1样式调整

--- src/scripts/controllers/find/hotFocusDetail.controller.js
+++ src/scripts/controllers/find/hotFocusDetail.controller.js
@@ -11,25 +11,80 @@ function HotFocusDetailController(loading) {
     vm.chartConfig1 = {
         options: {
             chart: {
-                type: 'line',
+                height: 250,
+            },
+            legend: {
+                enabled: false,
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, 'rgba(231, 239, 243, 0.35)'],
+                            [1, 'rgba(231, 239, 243, 0.50)']
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 2,
+                    pointPlacement: 'on',
+                    lineColor: '#5A626D',
+                    states: {
+                        hover: {
+                            lineWidth: 2,
+                        }
+                    },
+                    threshold: null
+                },
+                series: {
+                    marker: {
+                        fillColor: '#5A626D',
+                    },
+                },
+            },
+            tooltip: {
+                backgroundColor: '#222C38',
+                borderRadius: 4,
+                borderWidth: 0,
+                style: {
+                    fontSize: '13px',
+                    color: '#fff',
+                    lineHeight: '16px',
+                    padding: '6px 14px',
+                },
+                padding: 14,
+                shadow: false,
+                useHTML: true,
             },
         },
         title: {
-            text: '同比增长',
+            text: null,
         },
         xAxis: {
+            title: null,
+            lineColor: '#F2F4F5',
+            gridLineColor: '#F2F4F5',
+            gridLineDashStyle: 'dashed',
             categories: ['二月', '三月', '四月', '五月', '六月', '七月', '八月'],
         },
         yAxis: {
             title: null,
+            lineColor: '#F2F4F5',
+            gridLineColor: '#F2F4F5',
+            gridLineDashStyle: 'dashed',
         },
         series: [{
-            name: '增长',
+            type: 'area',
+            name: '热度',
             data: [10, 13, 25, 3, 8, 30, 15],
         }],
-        legend: {
-            enabled: false,
-        },
     };
     // 图表 2 配置
     vm.chartConfig2 = {
