4939a6dfeea83e7ee9f1835c43c60e3d41bd9e19
sweeetcc <sweeetcc@gmail.com>
2017-03-25 16:20:30 +0800

    feat: 机构在融样式

--- src/scripts/controllers/find/projectAlbum.controller.js
+++ src/scripts/controllers/find/projectAlbum.controller.js
@@ -12,12 +12,19 @@ function ProjectAlbumController(loading, ErrorService, FindService, $stateParams
     switch(vm.params.type) {
         case 'rong':
             document.title = '机构在融';
+            vm.title = '机构在融';
             break;
         case 'hunting':
             document.title = 'HUNTING+';
+            vm.title = 'HUNTING+';
             break;
         case 'timer':
             document.title = '限时首发';
+            vm.title = '限时首发';
             break;
     }
+    // 页面初始化
+    loading.show('projectAlbumLoading');
+    loading.hide('projectAlbumLoading');
+    vm.text = "在多位业内人士看来，美图将上市地点选在中国香港，或与国内资本市场的上市门槛有关。由于美图公司尚未盈利，目前其投资公司的资金存续期也到了一定阶段。";
 }
