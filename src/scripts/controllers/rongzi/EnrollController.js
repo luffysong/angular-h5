bc9a679f0154ed70a10486088bc7823a30ceef2b
zhangliansheng <zls0926@gmail.com>
2017-04-03 20:56:38 +0800

    feat: 添加报名成功提示框

--- src/scripts/controllers/rongzi/EnrollController.js
+++ src/scripts/controllers/rongzi/EnrollController.js
@@ -2,11 +2,12 @@ var angular = require('angular');
 angular.module('defaultApp.controller')
   .controller('EnrollController', EnrollController);
 
-function EnrollController(loading, $stateParams, RongziService, $state, UserService, ErrorService) {
+function EnrollController(loading, $stateParams, RongziService, $state, UserService, ErrorService, $modal) {
     var vm = this;
     vm.projectList = [];
     vm.enrollEvent = enrollEvent;
     vm.isLogin = false;
+    vm.modalOpen = modalOpen;
     init();
     function init() {
         removeHeader();
@@ -29,23 +30,62 @@ function EnrollController(loading, $stateParams, RongziService, $state, UserServ
     }
 
     function enrollEvent(ccid) {
+                vm.modalOpen();
+
         params = {
             ccid: ccid
         }
         RongziService.enroll(params)
             .then(function (response) {
-                angular.forEach(this.projectList, function (o) {
+                console.log(ccid);
+                angular.forEach(vm.projectList, function (o) {
                   if (o.ccid === ccid) {
+                    console.log(o);
                     o.projectId = response.data.projectId;
                     o.applied = true;
+                    console.log(o);
+                    console.log('=====================');
                   }
                 });
             }).catch(fail);
     }
 
+    function modalOpen() {
+        var info = {
+            title: '已报名成功！',
+            content: '点击查看分享页，可分享给好友为你点赞！'
+        };
+        $modal.open({
+            templateUrl: 'templates/rongzi-common/enrollMessage.html',
+            windowClass: 'nativeAlert_wrap',
+            controller: modalController,
+            controllerAs: 'vm',
+            resolve: {
+                obj: function () {
+                    return info;
+                }
+            }
+        });
+    }
+
+    modalController.$inject = ['$modalInstance', 'obj'];
+    function modalController($modalInstance, obj) {
+        var vm = this;
+        vm.title = obj.title;
+        vm.content = obj.content;
+        vm.cancelModal = cancelModal;
+
+        function cancelModal() {
+            $modalInstance.dismiss();
+        }
+
+    }
+
+
     function initTitle(t) {
         document.title = t;
     }
+
 
     function fail(err) {
         ErrorService.alert(err.err.msg);
