3d817a1014140cfff37b7d51f0bd5ca04ef6d9ff
songyongqiang <songyongqiang@36kr.com>
2017-04-10 19:34:09 +0800

    fix:榜单页面修改

--- src/scripts/controllers/rongzi/BestListController.js
+++ src/scripts/controllers/rongzi/BestListController.js
@@ -22,7 +22,6 @@ function BestListController($modal, loading, $stateParams, FindService,
     function init() {
         initUserInfo();
         loading.hide('findLoading');
-        initData();
         initWeixin();
         initTitle();
         if (!hybrid.isInApp) {
@@ -71,6 +70,14 @@ function BestListController($modal, loading, $stateParams, FindService,
         };
         RongziService.getProList(sendata)
             .then(function setProList(response) {
+                    if (!vm.top3) {
+                        var spliArr = response.data.data.splice(0, 3);
+                        vm.top3 = [];
+                        vm.top3[0] = spliArr[1];
+                        vm.top3[1] = spliArr[0];
+                        vm.top3[2] = spliArr[2];
+                    }
+
                     vm.prolist = vm.prolist.concat(response.data.data);
                     if (response.data.totalPages) {
                         vm.page = response.data.page || 0;
--- src/scripts/controllers/rongzi/OrgDetailController.js
+++ src/scripts/controllers/rongzi/OrgDetailController.js
@@ -10,11 +10,17 @@ function OrgDetailController($modal, loading, $stateParams, RongziService, $stat
     vm.category = $stateParams.category;
     vm.name = $stateParams.name;
     vm.nameArr = [];
+    vm.subscribe = subscribe;
+    vm.needApp = true;
+    vm.hasEmail = false;
+    vm.openApp = openApp;
+    vm.openAppUrl;
     init();
 
     function init() {
         loading.hide('findLoading');
         initData();
+        initLinkme();
         initTitle('融资季·' + $stateParams.name);
         vm.nameArr = vm.name.split('');
     }
@@ -54,4 +60,170 @@ function OrgDetailController($modal, loading, $stateParams, RongziService, $stat
         ErrorService.alert(err.err.msg);
     }
 
+    function subscribe(item, e) {
+        e.preventDefault();
+        e.stopPropagation();
+        item = item ? item : {};
+        item.investRole = vm.investRole;
+        item.hasEmail = vm.hasEmail;
+        if (!hybrid.isInApp) {
+            //document.location.href = vm.openAppUrl;
+            defaultModal();
+        }else if (hybrid.isInApp && vm.result.remind === 1 && UserService.getUID()) {
+            subscribeAction(item);
+        }else if (UserService.getUID() && vm.result.remind === 0 && UserService.getUID()) {
+            cancelSubscribeAction(item);
+        } else {
+            window.location.href = 'https://passport.36kr.com/pages';
+        }
+    }
+
+    modalController.$inject = ['$modalInstance', 'obj', 'hybrid'];
+    function modalController($modalInstance, obj, hybrid) {
+
+        var vm = this;
+        vm.item = obj;
+        vm.cancelModal = cancelModal;
+        vm.title = obj.title;
+        vm.investRole = obj.investRole;
+        vm.hasEmail = obj.hasEmail;
+        vm.cancelMainRemind = obj.cancelMainRemind;
+        vm.cancelRemindtxt = obj.cancelRemindtxt;
+        vm.needApp = true;
+        vm.setEmailState = false;
+        vm.addEmail = addEmail;
+
+        function cancelModal() {
+            $modalInstance.dismiss();
+        }
+
+        function addEmail() {
+            var senddata = {
+                email: vm.emailInput,
+            };
+            if (vm.emailInput) {
+                RongziService.setEmail(senddata)
+                .then(function setSussess() {
+                    vm.title = '添加邮件提醒成功！';
+                    vm.setEmailState = true;
+                    vm.hasEmail = true;
+                })
+              .catch(fail);
+            } else {
+                $modalInstance.dismiss();
+            }
+        }
+    }
+
+    function subscribeAction(item) {
+        var senddata = {
+            id:vm.result.id,
+            category:vm.result.category,
+            subscibeType:0,
+        };
+        RongziService.setSubscribe(senddata)
+        .then(function setSussess(data) {
+            if (data.data.data) {
+                vm.hasEmail = true;
+            }
+
+            item.hasEmail = vm.hasEmail;
+            item.cancelMainRemind = false;
+            item.title = '设置开场提醒成功！';
+            modalOpen(item);
+            vm.result.remind = 0;
+        })
+        .catch(fail);
+    }
+
+    function cancelSubscribeAction(item) {
+        var senddata = {
+            id:vm.result.id,
+            category:vm.result.category,
+            subscibeType:0,
+        };
+        RongziService.cancelSubscribe(senddata)
+        .then(function setSussess() {
+            item.cancelMainRemind = true;
+            item.title = '取消开场提醒成功！';
+            item.hasEmail = true;
+            item.cancelRemindtxt = '后续新上的顶级机构专场将不会有专场提醒，现已有排期的专场仍会提醒！';
+            modalOpen(item);
+            vm.result.remind = 1;
+        })
+        .catch(fail);
+    }
+
+    function modalOpen(item) {
+        $modal.open({
+            templateUrl: 'templates/rongzi-common/remindAlert.html',
+            windowClass: 'nativeAlert_wrap',
+            controller: modalController,
+            controllerAs: 'vm',
+            resolve: {
+                obj: function () {
+                    return item;
+                }
+            }
+        });
+    }
+
+    function openApp() {
+        if (!hybrid.isInApp) {
+            defaultModal();
+        }
+
+    }
+
+    function defaultModal(item) {
+        item = item ? item : {};
+        item.openUrl = vm.openAppUrl;
+        $modal.open({
+            templateUrl: 'templates/rongzi-common/downloadApp.html',
+            windowClass: 'nativeAlert_wrap',
+            controller: defaultController,
+            controllerAs: 'vm',
+            resolve: {
+                obj: function () {
+                    return item;
+                }
+            }
+        });
+    }
+
+    defaultController.$inject = ['$modalInstance', 'obj'];
+    function defaultController($modalInstance, obj) {
+
+        var vm = this;
+        vm.openUrl = obj.openUrl;
+        vm.cancelModal = cancelModal;
+
+        function cancelModal() {
+            $modalInstance.dismiss();
+        }
+    }
+
+    function initLinkme() {
+        var krdata = {};
+        krdata.type =  window.projectEnvConfig.linkmeType;
+        krdata.params =
+        '{"openlink":"https://' + window.projectEnvConfig.rongHost + '/m/#/rongzi/orgdetail?id=' + $stateParams.id + '&name=' + $stateParams.name + '&category=' + $stateParams.category + '","currentRoom":"0"}';
+        window.linkedme.init(window.projectEnvConfig.linkmeKey,
+        { type: window.projectEnvConfig.linkmeType }, function (err, res) {
+                if (err) {
+                    return;
+                }
+
+                window.linkedme.link(krdata, function (err, data) {
+                        if (err) {
+                            // 生成深度链接失败，返回错误对象err
+                            console.log(err);
+                        } else {
+                            // 生成深度链接成功，深度链接可以通过data.url得到
+                            //console.log(data.url,   $('#comm-openApp11').attr('href'));
+                            vm.openAppUrl = data.url;
+                        }
+                    }, false);
+            });
+    }
 }
