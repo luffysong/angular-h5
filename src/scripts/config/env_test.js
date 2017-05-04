814654fe161b2fbd92c8364425da608501aaf78d
songyongqiang <songyongqiang@36kr.com>
2017-05-04 17:15:28 +0800

    fix:01环境添加linkme

--- src/scripts/config/env_test.js
+++ src/scripts/config/env_test.js
@@ -6,5 +6,7 @@ window.projectEnvConfig = {
     jrHost:'test01.36jr.com',
     ucHost: 'uctest01.36kr.com',
     helpHost: 'helptest01.36kr.com',
-    nrongHost: 'nrongtest01.36kr.com'
+    nrongHost: 'nrongtest01.36kr.com',
+    linkmeKey: '3a89d6c23e6988e0e600d63ca3c70636',
+    linkmeType: 'test'
 };
