0fdd6cb3fa25c8efa8324413f96ce7a2d797a864
songyongqiang <songyongqiang@36kr.com>
2017-05-08 17:24:02 +0800

    feat:榜单分享页面

--- src/scripts/controllers/bangdan/BangdanOrgDetailController.js
+++ src/scripts/controllers/bangdan/BangdanOrgDetailController.js
@@ -4,5 +4,34 @@ angular.module('defaultApp.controller')
 
 function BangdanOrgDetailController(loading, $scope, $modal, $stateParams, FindService,
     $state, UserService, RongziService, ErrorService, hybrid, $rootScope, $timeout) {
+    var vm = this;
+    vm.joinpro = joinpro;
+
+    function joinpro(item) {
+        item = item ? item : {};
+        $modal.open({
+                templateUrl: 'templates/bangdan/shareWin.html',
+                windowClass: 'bd-nativeAlert_wrap',
+                controller: defaultController,
+                controllerAs: 'vm',
+                resolve: {
+                    obj: function () {
+                        return item;
+                    }
+                }
+            });
+    }
+
+    defaultController.$inject = ['$modalInstance', 'obj'];
+
+    function defaultController($modalInstance, obj) {
+
+        var vm = this;
+        vm.cancelModal = cancelModal;
+
+        function cancelModal() {
+            $modalInstance.dismiss();
+        }
+    }
 
 }
