95bf51aa4a31aaec2aa14d1757f4c9dd69ec2e48
songyongqiang <songyongqiang@36kr.com>
2017-05-11 22:27:03 +0800

    fix:微信原生分享

--- src/scripts/controllers/bangdan/BangdanOrgDetailController.js
+++ src/scripts/controllers/bangdan/BangdanOrgDetailController.js
@@ -27,11 +27,12 @@ function BangdanOrgDetailController(loading, $scope, $modal, $stateParams,
 
         getProList();
         getQ();
+        initH5();
         var HOST = location.host;
         var shareUrl =
         'https://' + HOST + '/m/#/bangdan/bdshare?id=' + $stateParams.id + '&rank=' + $stateParams.rank;
         initWeixin(vm.orgInfo.name, vm.orgInfo.projectCount, vm.currQuarter, vm.rank, shareUrl, vm.orgInfo.logo);
-        initWeixinH5();
+
     }
 
     function getQ() {
@@ -351,7 +352,7 @@ function BangdanOrgDetailController(loading, $scope, $modal, $stateParams,
         }
     }
 
-    function initWeixinH5() {
+    function initH5() {
         var signature = '';
         var nonceStr = 'xcvdsjlk$klsc';
         var timestamp = parseInt(new Date().getTime() / 1000);
@@ -362,7 +363,7 @@ function BangdanOrgDetailController(loading, $scope, $modal, $stateParams,
             noncestr: nonceStr
         }, function (data) {
             wx.config({
-                debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: 'wxd3ea1a9a22815a8c', // 必填，公众号的唯一标识
                 timestamp: timestamp, // 必填，生成签名的时间戳
                 nonceStr: nonceStr, // 必填，生成签名的随机串
@@ -415,13 +416,13 @@ function BangdanOrgDetailController(loading, $scope, $modal, $stateParams,
             });
 
             wx.error(function (res) {
+                alert(JSON.stringify(res, null, 4));
 
-                // alert(JSON.stringify(res, null, 4));
                 // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
 
             });
 
         }, 'jsonp');
-    };
+    }
 
 }
