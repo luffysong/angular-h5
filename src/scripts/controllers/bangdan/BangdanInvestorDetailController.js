5e7642b27e80a5115186bf37ca6ac82151189243
songyongqiang <songyongqiang@36kr.com>
2017-05-18 17:29:46 +0800

    feat:投资人榜单展开信息

--- src/scripts/controllers/bangdan/BangdanInvestorDetailController.js
+++ src/scripts/controllers/bangdan/BangdanInvestorDetailController.js
@@ -9,7 +9,10 @@ function BangdanInvestorDetailController(loading, $scope, $modal, $stateParams, 
     vm.busy = false;
     vm.prolist = [];
     vm.rank = '1';
+    vm.openMore = openMore;
     vm.intro = '而我却二而我却二而我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二';
+    vm.industry = '文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱·文娱';
+    vm.collapse = true;
     init();
 
     function init() {
@@ -22,7 +25,7 @@ function BangdanInvestorDetailController(loading, $scope, $modal, $stateParams, 
 
         var params = {
             page: vm.page + 1,
-            pageSize: 10,
+            pageSize: 20,
         };
 
         BangDanService.getOrgProRank($stateParams.id, params)
@@ -43,6 +46,42 @@ function BangdanInvestorDetailController(loading, $scope, $modal, $stateParams, 
 
         })
         .catch(fail);
+    }
+
+    function openMore(e) {
+        var obj = angular.element(e.currentTarget);
+        var openClose = obj.find('.open-close');
+
+        var $infoObj = obj.parent().find('.info-content').find('.info').find('.text-content');
+        var $indObj = obj.parent().find('.info-content').find('.industry').find('.in-right').find('.text-content');
+        var lineClamp  = '-webkit-line-clamp';
+        var fhclmp = $infoObj.attr('cla-height');
+        var ihclmp = $indObj.attr('cla-height');
+        var fh = $infoObj.attr('o-height');
+        var ih = $indObj.attr('o-height');
+
+        if (vm.collapse) {
+            $infoObj.css(lineClamp, '');
+            $infoObj.css('transition', 'height 0.3s');
+            $infoObj.css('height', fh + 'px');
+
+            $indObj.css(lineClamp, '');
+            $indObj.css('transition', 'height 0.3s');
+            $indObj.css('height', ih + 'px');
+            openClose.css('transform', 'rotate(180deg)');
+            vm.collapse = false;
+        }else {
+            $infoObj.css('transition', 'height 0.3s');
+            $infoObj.css('height',  fhclmp + 'px');
+            $infoObj.css(lineClamp, '4');
+            $indObj.css(lineClamp, '2');
+
+            $indObj.css('transition', 'height 0.3s');
+            $indObj.css('height',  ihclmp + 'px');
+            openClose.css('transform', '');
+            vm.collapse = true;
+        }
+
     }
 
     function fail(err) {
--- src/scripts/directives/TextLineClampDirecitive.js
+++ src/scripts/directives/TextLineClampDirecitive.js
@@ -27,9 +27,11 @@ angular.module('defaultApp.directive').directive('textLineClamp',
                 var $toggle = element.find('.text-toggle');
                 var height = $textContent[0].offsetHeight;
                 var lineClamp  = '-webkit-line-clamp';
+                $textContent.attr('o-height', height);
                 $textContent.css('text-overflow', 'ellipsis');
                 $textContent.css(lineClamp, vm.lineClamp);
                 var clampHeight = $textContent[0].offsetHeight;
+                $textContent.attr('cla-height', clampHeight);
                 $textContent.css('height', clampHeight + 'px');
                 if (clampHeight === height) {
                     element.find('.text-toggle').hide();
@@ -49,10 +51,10 @@ angular.module('defaultApp.directive').directive('textLineClamp',
                 });
 
                 $textContent[0].addEventListener('webkitTransitionEnd', function webkitTransitionEnd() {
-                    if (vm.collapse) {
+                    if (vm.collapse && $toggle[0]) {
                         $toggle[0].innerHTML = '收起';
                         vm.collapse = false;
-                    }else {
+                    }else if ($toggle[0]) {
                         $toggle[0].innerHTML = '展开';
                         vm.collapse = true;
                         $textContent.css(lineClamp, vm.lineClamp);
