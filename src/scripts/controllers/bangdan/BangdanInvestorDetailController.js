75d19300959df9338e09f979606478244177ae25
songyongqiang <songyongqiang@36kr.com>
2017-05-18 15:17:13 +0800

    feat:投资人详情

--- src/scripts/controllers/bangdan/BangdanInvestorDetailController.js
+++ src/scripts/controllers/bangdan/BangdanInvestorDetailController.js
@@ -7,5 +7,51 @@ function BangdanInvestorDetailController(loading, $scope, $modal, $stateParams, 
     var vm = this;
     vm.page = 0;
     vm.busy = false;
+    vm.prolist = [];
+    vm.rank = '1';
+    vm.intro = '而我却二而我却二而我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二而我却二';
+    init();
+
+    function init() {
+        getProList();
+    }
+
+    function getProList() {
+        if (vm.busy) return;
+        vm.busy = true;
+
+        var params = {
+            page: vm.page + 1,
+            pageSize: 10,
+        };
+
+        BangDanService.getOrgProRank($stateParams.id, params)
+        .then(function setdata(response) {
+            vm.startloading = false;
+            loading.hide('bangdanDetailLoading');
+            vm.prolist = vm.prolist.concat(response.data.data);
+            if (response.data.totalPages) {
+                vm.page = response.data.page || 0;
+
+                if (response.data.totalPages !== vm.page && response.data.data.length > 0) {
+                    vm.busy = false;
+                } else {
+                    vm.finish = true;
+                    vm.more = true;
+                }
+            }
+
+        })
+        .catch(fail);
+    }
+
+    function fail(err) {
+        loading.hide('bangdanDetailLoading');
+        if (err.code === '403') {
+            console.log(err.msg);
+        } else if (err.code === '1') {
+            ErrorService.alert(err.msg);
+        }
+    }
 
 }
