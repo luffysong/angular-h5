546210bd9bc2ea5974ffd3fc0369ca3f64a48ad7
wy <wy@shujujiance.com>
2017-06-19 16:57:03 +0800

    fix：行研搜索顶部固定

--- src/scripts/controllers/find/industryReportList.controller.js
+++ src/scripts/controllers/find/industryReportList.controller.js
@@ -134,6 +134,7 @@ function IndustryReportListController(FindService, ErrorService, loading, hybrid
         vm.endLoading = true;
         vm.searchKey = '取消';
         $('#block-part-con').css("display","none");
+        $('.block-part-div').css("position","fixed");
     }
     function searchBlur(e) {
         e.preventDefault();
